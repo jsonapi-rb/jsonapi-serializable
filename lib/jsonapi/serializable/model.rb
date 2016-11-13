19ef44961536495bd5b5240f42a874f0cef33265
Lucas Hosseini <lucas.hosseini@gmail.com>
2016-11-14 00:36:41 +0100

    Allow lazy resource class name of related model. Also forward parameters to related resources, fix serializable resource class inferrence, and remove api versionning.

--- lib/jsonapi/serializable/model.rb
+++ lib/jsonapi/serializable/model.rb
@@ -6,24 +6,14 @@ module JSONAPI
     class Model < Resource
       include ModelDSL
 
-      class << self
-        attr_accessor :api_version_val
-      end
-
-      def self.inherited(klass)
-        super
-        klass.api_version_val = api_version_val
-      end
-
       id { @model.public_send(:id).to_s }
 
-      def resource_klass_for(model_klass)
-        names = model_klass.name.split('::'.freeze)
-        model_klass_name = names.pop
+      def self.resource_klass_for(model_klass_name)
+        names = model_klass_name.split('::'.freeze)
+        klass_name = names.pop
         namespace = names.join('::'.freeze)
-        version = self.class.api_version_val
 
-        klass_name = [namespace, version, "Serializable#{model_klass_name}"]
+        klass_name = [namespace, "Serializable#{klass_name}"]
                      .reject(&:nil?)
                      .reject(&:empty?)
                      .join('::'.freeze)
--- lib/jsonapi/serializable/model_dsl.rb
+++ lib/jsonapi/serializable/model_dsl.rb
@@ -6,28 +6,20 @@ module JSONAPI
       end
 
       module ClassMethods
-        def api_version(value)
-          self.api_version_val = value
-        end
-
-        def type(value = nil)
-          value ||= name
-          super(value)
-        end
-
         def attribute(attr, &block)
           block ||= proc { @model.public_send(attr) }
           super(attr, &block)
         end
 
-        def has_many(rel, resource_klass = nil, &block)
+        def has_many(rel, resource_class = nil, &block)
+          if resource_class.is_a?(String)
+            resource_class = Object.const_get(resource_class)
+          end
           rel_block = proc do
-            if resource_klass
-              data do
-                @model.public_send(rel).map do |related|
-                  resource_klass ||= resource_klass_for(related.class)
-                  resource_klass.new(model: related)
-                end
+            data do
+              @model.public_send(rel).map do |related|
+                resource_class ||= Model.resource_klass_for(related.class.name)
+                resource_class.new(@_param_hash.merge(model: related))
               end
             end
             instance_eval(&block) unless block.nil?
@@ -35,13 +27,18 @@ module JSONAPI
           relationship(rel, &rel_block)
         end
 
-        def has_one(rel, resource_klass = nil, &block)
+        def has_one(rel, resource_class = nil, &block)
+          if resource_class.is_a?(String)
+            resource_class = Object.const_get(resource_class)
+          end
           rel_block = proc do
-            if resource_klass
-              data do
-                related = @model.public_send(rel)
-                resource_klass ||= resource_klass_for(related.class)
-                resource_klass.new(model: related)
+            data do
+              related = @model.public_send(rel)
+              if related.nil?
+                nil
+              else
+                resource_class ||= Model.resource_klass_for(related.class.name)
+                resource_class.new(@_param_hash.merge(model: related))
               end
             end
             instance_eval(&block) unless block.nil?
