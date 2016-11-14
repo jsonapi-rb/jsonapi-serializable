e660a2a3620f1e48111c0a9c359ab6135e91da7b
Lucas Hosseini <lucas.hosseini@gmail.com>
2016-11-14 01:28:11 +0100

    Remove linkage_data over-optimization. (#2)

--- lib/jsonapi/serializable/relationship.rb
+++ lib/jsonapi/serializable/relationship.rb
@@ -18,24 +18,19 @@ module JSONAPI
         hash[:links] = @_links if @_links.any?
         hash[:meta] = @_meta unless @_meta.nil?
         return hash unless included || (!@_links.any? && @_meta.nil?)
-        hash[:data] = eval_linkage_data
+        hash[:data] = linkage_data
 
         hash
       end
 
       private
 
-      def eval_linkage_data
-        @_linkage_data ||=
-          if @_linkage_data_block
-            @_linkage_data_block.call
-          elsif data.respond_to?(:each)
-            data.map { |res| { type: res.jsonapi_type, id: res.jsonapi_id } }
-          elsif data.nil?
-            nil
-          else
-            { type: data.jsonapi_type, id: data.jsonapi_id }
-          end
+      def linkage_data
+        linkage_data = Array(data).map do |res|
+          { type: res.jsonapi_type, id: res.jsonapi_id }
+        end
+
+        data.respond_to?(:each) ? linkage_data : linkage_data.first
       end
     end
   end
