2d170eab56d4eb50bdf7a71843d470aa0a3dfcbc
Lucas <lucas@Lucass-MacBook-Air.local>
2017-06-04 02:39:14 +0200

    Add yarddoc to renderers.

--- lib/jsonapi/serializable/renderer.rb
+++ lib/jsonapi/serializable/renderer.rb
@@ -4,6 +4,39 @@ require 'jsonapi/serializable/resource_builder'
 module JSONAPI
   module Serializable
     class SuccessRenderer
+      # Serialize resources into a JSON API document.
+      #
+      # @param [nil,Object,Array] resources
+      # @param [Hash] options @see JSONAPI.render
+      # @option [Class,Symbol,String,Hash{Symbol,String=>Class,Symbol,String}]
+      #   class The serializable resource class(es) to use for the primary
+      #   resources.
+      # @option [String] namespace The namespace in which to look for
+      #   serializable resource classes.
+      # @option [#call] inferrer The callable used for inferring a serializable
+      #   resource class name from a resource class name.
+      # @option [Hash] expose The exposures made available in serializable
+      #   resource class instances as instance variables.
+      # @return [Hash]
+      #
+      # @example
+      #   JSONAPI.serialize(nil)
+      #   # => { data: nil }
+      #
+      # @example
+      #   JSONAPI.serialize(user)
+      #   # => {
+      #          data: {
+      #            type: 'users',
+      #            id: 'foo',
+      #            attributes: { ... },
+      #            relationships: { ... }
+      #          }
+      #        }
+      #
+      # @example
+      #   JSONAPI.serialize([user1, user2])
+      #   # => { data: [{ type: 'users', id: 'foo', ... }, ...] }
       def render(resources, options = {})
         options   = options.dup
         klass     = options.delete(:class)
@@ -31,6 +64,16 @@ module JSONAPI
     end
 
     class ErrorRenderer
+      # Serialize errors into a JSON API document.
+      #
+      # @param [Array] errors
+      # @param [Hash] options @see JSONAPI.render
+      # @return [Hash]
+      #
+      # @example
+      #   error = JSONAPI::Serializable::Error.create(id: 'foo', title: 'bar')
+      #   JSONAPI.serialize_errors([error])
+      #   # => { errors: [{ id: 'foo', title: 'bar' }] }
       def render(errors, options = {})
         JSONAPI.render(options.merge(errors: errors))
       end
