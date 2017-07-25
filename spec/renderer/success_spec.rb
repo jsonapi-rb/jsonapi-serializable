6287af47a5928e91d09041f235444f24ef39701f
Lucas <lucas.hosseini@gmail.com>
2017-07-25 07:30:20 +0200

    Add tests for inferrers.

--- spec/renderer/success_spec.rb
+++ spec/renderer/success_spec.rb
@@ -29,4 +29,38 @@ describe JSONAPI::Serializable::SuccessRenderer, '#render' do
     expect(hash[:data][:type]).to eq(:api_users)
     expect(hash[:included][0][:type]).to eq(:api_posts)
   end
+
+  context 'when providing a custom explicit inferrer' do
+    it 'uses the inferred serializable classes' do
+      inferrer = {
+        User: 'API::SerializableUser',
+        Post: 'API::SerializablePost'
+      }
+      hash = subject.render(user, include: [:posts], inferrer: inferrer)
+
+      expect(hash[:data][:type]).to eq(:api_users)
+      expect(hash[:included][0][:type]).to eq(:api_posts)
+    end
+  end
+
+  context 'when providing a custom implicit inferrer' do
+    it 'uses the inferred serializable classes' do
+      inferrer = Hash.new { |h, k| h[k] = "API::Serializable#{k}" }
+      hash = subject.render(user, include: [:posts], inferrer: inferrer)
+
+      expect(hash[:data][:type]).to eq(:api_users)
+      expect(hash[:included][0][:type]).to eq(:api_posts)
+    end
+  end
+
+  context 'when providing a custom inferrer and a namespace' do
+    it 'uses the inferred serializable classes' do
+      inferrer = Hash.new { |h, k| h[k] = "Serializable#{k}" }
+      hash = subject.render(user, include: [:posts],
+                            inferrer: inferrer, namespace: 'API')
+
+      expect(hash[:data][:type]).to eq(:api_users)
+      expect(hash[:included][0][:type]).to eq(:api_posts)
+    end
+  end
 end
